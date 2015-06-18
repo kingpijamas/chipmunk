e986987534d676130ed79c5e308e9df85885e2e1
kpjjpk <juanpabloreyv@gmail.com>
2015-06-18 18:35:08 -0300

    Add relation declaration utility methods Add joda-time converters Clean up build.gradle

--- src/main/scala/org/chipmunk/SplittableSchema.scala
+++ src/main/scala/org/chipmunk/SplittableSchema.scala
@@ -1,11 +1,16 @@
 package org.chipmunk
 
+import org.squeryl.PrimitiveTypeMode.long2ScalarLong
+import org.squeryl.PrimitiveTypeMode.manyToManyRelation
+import org.squeryl.PrimitiveTypeMode.oneToManyRelation
 import scala.collection.mutable
 import org.squeryl.Schema
 import org.squeryl.dsl.{ Relation => SquerylRelation }
 import org.squeryl.Table
 import org.squeryl.dsl.{ Relation => SquerylRelation }
 import org.squeryl.dsl.{ Relation => SquerylRelation }
+import org.squeryl.dsl.ManyToManyRelation
+import org.squeryl.dsl.ast.EqualityExpression
 
 trait SplittableSchema extends Schema {
   private[this] var relationDeclarations = mutable.Buffer[DeclaredRelation[_]]()
@@ -20,6 +25,21 @@ trait SplittableSchema extends Schema {
     val declaration = new DeclaredRelation(init)
     relationDeclarations += declaration
     declaration
+  }
+
+  protected def manyToMany[L <: persistent.Entity[_], R <: persistent.Entity[_]](
+    tableL: Table[L], tableR: Table[R], nameOfMiddleTable: String): DeclaredRelation[SquerylRelation[L, R]] = {
+    relation {
+      manyToManyRelation(tableL, tableR, nameOfMiddleTable).via[BinaryAssociation](
+        (l, r, a) => (l.id === a.ownerId, r.id === a.owneeId))
+    }
+  }
+
+  protected def oneToMany[O <: persistent.Entity[_], M](
+    tableO: Table[O], tableM: Table[M], f: (O, M) => EqualityExpression): DeclaredRelation[SquerylRelation[O, M]] = {
+    relation {
+      oneToManyRelation(tableO, tableM).via(f)
+    }
   }
 
   def initRelations(): Unit = {
