dc0263e59f5a8922bba1d95956eb3685ce42c1e0
kpjjpk <juanpabloreyv@gmail.com>
2015-08-30 17:07:35 -0300

    Add tests for O2M and M2O

---
+++ src/test/scala/org/chipmunk/entity/relation/ManyToOneSpec.scala
@@ -0,0 +1,47 @@
+package org.chipmunk.entity.relation
+
+import org.chipmunk.TestSchema.Animal
+import org.chipmunk.entity.Identifiable
+import org.chipmunk.entity.relation.mock.Query
+import org.mockito.Mockito.verify
+import org.scalatest.Finders
+import org.scalatest.fixture
+import org.scalatest.mock.MockitoSugar
+import org.squeryl.dsl.{ ManyToOne => SM2O }
+import org.squeryl.KeyedEntity
+
+class ManyToOneSpec extends fixture.FlatSpec with MockitoSugar {
+  "A ManyToOne" should "call 'assign' on +=" in { f =>
+    val animal = new Animal
+    val innerRel = f.m2o.toSqueryl
+
+    f.m2o += animal
+    verify(innerRel).assign(animal)
+  }
+
+  it should "call 'delete' on clear" in { f =>
+    val animal = new Animal
+    val innerRel = f.m2o.toSqueryl
+
+    f.m2o.clear()
+    verify(innerRel).delete
+  }
+
+  protected def withFixture(test: OneArgTest) = {
+    val owner = new TestManyToOne
+    val fixture = FixtureParam(owner)
+    withFixture(test.toNoArgTest(fixture))
+  }
+
+  case class FixtureParam(m2o: ManyToOne[Animal])
+
+  class TestManyToOne extends ManyToOne[Animal] {
+    val toSqueryl: SRel = mock[TestSM2O[Animal]]
+  }
+
+  class TestSM2O[O <: Identifiable] extends Query[O] with SM2O[O] {
+    def assign(one: O): O = ???
+    def delete: Boolean = ???
+    def iterable: Iterable[O] = ???
+  }
+}
---
+++ src/test/scala/org/chipmunk/entity/relation/OneToManySpec.scala
@@ -0,0 +1,48 @@
+package org.chipmunk.entity.relation
+
+import org.chipmunk.TestSchema.Animal
+import org.chipmunk.entity.Identifiable
+import org.chipmunk.entity.relation.mock.Query
+import org.mockito.Mockito.verify
+import org.scalatest.Finders
+import org.scalatest.fixture
+import org.scalatest.mock.MockitoSugar
+import org.squeryl.dsl.{ OneToMany => SO2M }
+import org.squeryl.KeyedEntity
+
+class OneToManySpec extends fixture.FlatSpec with MockitoSugar {
+  "A OneToMany" should "call 'associate' on +=" in { f =>
+    val animal = new Animal
+    val innerRel = f.o2m.toSqueryl
+
+    f.o2m += animal
+    verify(innerRel).associate(animal)
+  }
+
+  it should "call 'deleteAll' on clear" in { f =>
+    val animal = new Animal
+    val innerRel = f.o2m.toSqueryl
+
+    f.o2m.clear()
+    verify(innerRel).deleteAll
+  }
+
+  protected def withFixture(test: OneArgTest) = {
+    val owner = new TestOneToMany
+    val fixture = FixtureParam(owner)
+    withFixture(test.toNoArgTest(fixture))
+  }
+
+  case class FixtureParam(o2m: OneToMany[Animal])
+
+  class TestOneToMany extends OneToMany[Animal] {
+    val toSqueryl: SRel = mock[TestSO2M[Animal]]
+  }
+
+  class TestSO2M[O <: Identifiable] extends Query[O] with SO2M[O] {
+    def assign(m: O): O = ???
+    def associate(m: O)(implicit ev: <:<[O, KeyedEntity[_]]): O = ???
+    def deleteAll: Int = ???
+    def iterable: Iterable[O] = ???
+  }
+}
